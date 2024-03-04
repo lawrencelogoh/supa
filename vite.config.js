4f308e2f2514374d4cfa4d392d699c1717aea024
Lawrence Logoh <lawrence.logoh@gmail.com>
2024-03-04 17:57:25 +0000

    Add basic auth

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import httpAuth from "vite-plugin-http-basic-auth";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      react(),
+      httpAuth(
+        [
+          {
+            username: "larry123",
+            password: "r53kFxRpoe",
+          },
+        ],
+        {
+          realm: env.VITE_AUTH_REALM,
+          useInServer: true, // True by default
+          useInPreview: true, // True by default
+        },
+      ),
+    ],
+  };
+});
